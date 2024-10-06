1c1098ef1792d20e05866322a3237a8127fb35d3
dan <dan.moraru@dawsoncollege.qc.ca>
2024-10-05 23:38:04 -0400

    setup proj

---
+++ server/db/db.js
@@ -0,0 +1,24 @@
+// const mongoose = require('mongoose');
+
+// async function connectDB(uri) {
+//   try {
+//     await mongoose.connect(uri);
+//     // eslint-disable-next-line no-console
+//     console.log('Connected to MongoDB');
+//   } catch (e) {
+//     console.error('Could not connect');
+//     process.exit();
+//   }
+// }
+
+// function closeDB() {
+//   mongoose.connection.close();
+//   // eslint-disable-next-line no-console
+//   console.log('Database connection has been closed');
+// }
+
+// function dropCollection(model){
+//   model.collection.drop();
+// }
+
+// module.exports = { connectDB, closeDB, dropCollection};
